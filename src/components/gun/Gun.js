b4846722957b5fb7ab3214c30ea12fe8ae3bd2a0
Sami un nobi samid <67954576+NobiSamid@users.noreply.github.com>
2021-09-26 14:31:12 +0600

    color fixed

--- src/components/gun/Gun.js
+++ src/components/gun/Gun.js
@@ -13,12 +13,12 @@ const Gun = (props) => {
                 <div className="card h-100 card-bg">
                     <img src={image} className="card-img-top rounded-start cart-image" alt="thumbnail"></img>
                     <div className="card-body">
-                        <h5 className="card-title">{name}</h5>
+                        <h3 className="card-title">{name}</h3>
                         <p className="card-text">Type: {type}</p>
                         <p className="card-text">Fire-rate: {fireRate}</p>
                         <p className="card-text">Magazine-size: {magazineSize} round</p>
                         <p className="card-text">Price: {price}$</p>
-                        <button onClick={()=> props.handleAddGun(props.gun)} className="btn btn-primary">{cartIcon} Add to list</button>
+                        <button onClick={()=> props.handleAddGun(props.gun)} className="btn btn-secondary">{cartIcon} Add to list</button>
                     </div>
                 </div>
             </div>
